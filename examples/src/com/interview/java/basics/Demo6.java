62ba9aeebf8cad6a89120d86d9388c998e377786
wangguiyong <1571337712@qq.com>
2022-12-02 21:46:15 +0800

    demo 6

---
+++ examples/src/com/interview/java/basics/Demo6.java
@@ -0,0 +1,10 @@
+package com.interview.java.basics;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        float f1 = 100000000.0f;
+        System.out.println(f1);
+        float f2 = 1.3e23f;
+        System.out.println(f2);
+    }
+}
